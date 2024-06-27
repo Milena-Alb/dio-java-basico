c9967b961fd9ac80d7c489a1f1e12edd3a5b683b
Milena Leticia <milenaleticia.15sa@gmail.com>
2024-06-27 19:32:07 -0300

    Testando variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       String nome = "Milena";
+       byte idade = 19;
+       int cep = 21970333;
+       long cpf = 12345678910L;
+       double salarioMinimo = 2500.78;
+       float pi = 3.14f;
     }
 }
