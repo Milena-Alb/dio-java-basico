de152d7a59c24a05b0aa6c013c09f5b9fd5c6bbe
Milena Leticia <milenaleticia.15sa@gmail.com>
2024-06-27 21:51:40 -0300

    "Adicionando outros tipos de variaveis"

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -6,5 +6,7 @@ public class TiposVariaveis {
        long cpf = 12345678910L;
        double salarioMinimo = 2500.78;
        float pi = 3.14f;
+       char sexo = 'F';
+       boolean casado = true;
     }
 }
